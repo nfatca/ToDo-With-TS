90cc37e16b0ce625069ce440785f61983d54f2be
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-12-04 02:25:41 +0300

    input

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -6,6 +6,16 @@ interface IInputForm {
 
 const InputForm: React.FC<IInputForm> = ({ addTodo }) => {
   const [task, setTask] = useState("");
+
+  const handleClick = () => {
+    if (task.trim() !== "") {
+      addTodo(task);
+      setTask("");
+    } else {
+      alert("fill input box");
+      setTask("");
+    }
+  };
   return (
     <div className="input-form">
       <input
@@ -13,8 +23,15 @@ const InputForm: React.FC<IInputForm> = ({ addTodo }) => {
         placeholder="Enter the todo..."
         type="text"
         maxLength={40}
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
       />
-      <button className="btn-hover btn-color" type="submit">
+      <button
+        className="btn-hover btn-color"
+        type="submit"
+        onClick={handleClick}
+        disabled={!task}
+      >
         Add New Todo
       </button>
     </div>
